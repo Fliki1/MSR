7d58f1ad3524528e891e2a0064bf3f6ab50d3531
Flik1 <leo.pelonero@hotmail.it>
2021-06-28 18:29:52 +0200

    :recycle: Refactor code

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ def remove_duplicates(urls):
 def check_url(urls):
     """Validate url presi in input, return lista url validi e non duplicati"""
     url_list = []
-    urls_not_duplicate = remove_duplicates(urls)  # remove duplicate urls
-    for url in urls_not_duplicate:
+    for url in urls:
         if parse(url).valid & url.endswith(".git"):
             url_list.append(url)
             logger.info("✔ " + url)
-            # print("✔ "+url)
+        else:   # evitabile questo else
+            logger.debug("❌ " + url)
     logger.info('Urls validati')
     return url_list
 
@@ -37,16 +37,17 @@ def driller(url):
 
 
 def get_git_urls():
-    """Domanda all'user i git da analizzare, return lista url leciti"""
+    """ Domanda all'user i git da analizzare, return lista url leciti """
     url_input = input("Enter Gits Repositories: ")
     urls = url_input.split(", ")
-    urls_validate = check_url(urls)  # git url ckeck
+    urls_not_duplicate = remove_duplicates(urls)  # remove duplicate urls
+    urls_validate = check_url(urls_not_duplicate)  # git url ckeck
     return urls_validate
 
 
 def log(verbos):
-    """Setto i parametri per gestire il file di log"""
-    logger.setLevel(logging.INFO)
+    """ Setto i parametri per gestire il file di log """
+    logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
     if verbos:
         # StreamHandler: console
@@ -59,20 +60,19 @@ def log(verbos):
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
-
 def arg_parse():
-    """Verifico la possibile chiamata verbose"""
+    """ Verifico la possibile chiamata verbose """
     parser = argparse.ArgumentParser(description="Script che implementa metriche per il M.S.R. : tipo1 ,tipo2...")
-    parser.add_argument("-v", "--verbose", help="Restituisce output verboso", action="store_true")
+    parser.add_argument("-v", "--verbose", help="restituisce output verboso", action="store_true")
     args = parser.parse_args()
     return args.verbose
+    # TODO: gestione chiamata di quale tipo di metrica si vuole eseguire
 
 if __name__ == "__main__":
-
-    # Log2: gestisce sia la console che il salvataggio dei log [-v] (diversi per modulo)
-    verb = arg_parse()  # args parse: verbose?
+    # Log: gestisce sia la console che il salvataggio dei log [-v] (diversi per modulo)
+    verb = arg_parse()  # args parse: verbose choise?
     log(verb)           # log file
-    logger.info('Inizio')
+    logger.info('Inizio del M.S.R.')
     urls = get_git_urls()
     # driller(urls)
-    logger.info('Fine')
+    logger.info('Fine del M.S.R.')
