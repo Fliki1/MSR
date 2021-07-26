402eb9e4e559be2d9d1018b454cd2292780d4df7
Flik1 <leo.pelonero@hotmail.it>
2021-07-26 13:10:47 +0200

    :sparkles: Average commit: add+remove range

--- src/AverageCommit.py
+++ src/AverageCommit.py
@@ -27,11 +27,11 @@ def log(verbos):
     logger.addHandler(file_handler)
 
 
-def bar_view(repo, repo_name, total_commits, average_file_type):
+def bar_view(repo, repo_name, total_commits, average_file_type, csv_headers):
     """ Average commit: bar console, non buono per benchmark visto il 0.1s di delay
         e anche il conteggio del calcolo: sum(1 for x in generator) oppure len(list(generator))"""
     # csv info
-    csv_headers = ["Commit_hash", "ADD+DEL"]
+    #csv_headers = ["Commit_hash", "ADD+DEL"]
     # average_file_type
     if average_file_type is not None:
         with open("./data-results/average_commit_" + repo_name + ".csv", 'w') as f:
@@ -69,10 +69,10 @@ def bar_view(repo, repo_name, total_commits, average_file_type):
         logger.info(f'Average Commit: {repo_name} ✔')
 
 
-def log_view(repo, repo_name, total_commits, average_file_type):
+def log_view(repo, repo_name, total_commits, average_file_type, csv_headers):
     """ Average commit: log console """
     # csv info
-    csv_headers = ["Commit_hash", "ADD+DEL"]
+    #csv_headers = ["Commit_hash", "ADD+DEL"]
     if average_file_type is not None:
         with open("./data-results/average_commit_" + repo_name + ".csv", 'w') as f:
             # Header del csv
@@ -120,7 +120,7 @@ def csv_sort(project_name):
     writer.writeheader()
     writer.writerows(result)
 
-def csv_avg(project_name):
+def csv_avg(project_name, csv_headers):
     # open the file in universal line ending mode
     with open("./data-results/average_commit_" + project_name + ".csv", 'rU') as infile:
         # read the file as a dictionary for each row ({header : value})
@@ -134,19 +134,46 @@ def csv_avg(project_name):
                     data[header] = [value]
 
     # extract the variables
-    int_list = data['ADD+DEL']              # calcolo della media: somma della lista/ len(int_list) arrotondato?
-    int_list = list(map(int, int_list))     # str list in int list
     hash_list = data['Commit_hash']
-    percentuale = round(len(int_list)*0.1)  # 10%
+    str_act_score = data['ADD+DEL']                 # calcolo della media: somma della lista/ len(int_list) arrotondato?
+    int_act_score = list(map(int, str_act_score))   # str list in int list
+
+    percentuale = round(len(int_act_score)*0.1)     # 10%
+    print('percentuale 10% ', percentuale)
     # prima bisogna levare gli estremi 10%, dalla risultante calcolarne la media
-    avg = round(sum(int_list[percentuale:-percentuale])/len(int_list[percentuale:-percentuale]))    # average
-    print(hash_list[percentuale:-percentuale])
+    avg = round(sum(int_act_score[percentuale:-percentuale])/len(int_act_score[percentuale:-percentuale]))    # average
+    print('activity score ', int_act_score[percentuale:-percentuale])
+    print('avg ', avg)
+    range = round(avg * 0.25)                       # range -25% + 25%
+    print('range ', range)
+
+    final_hash = []
+    final_score = []
+    for index, act_score in enumerate(int_act_score[percentuale:-percentuale]):
+        if act_score >= avg-range and act_score <= avg+range:
+            final_hash.append(hash_list[percentuale:-percentuale][index])
+            final_score.append(int_act_score[percentuale:-percentuale][index])
+    print(final_hash, final_score)
+
+    wow = len(final_score)
+    print(wow)
+    with open("./final-results/average_commit_" + project_name + ".csv", 'w') as f:
+        # Header del csv
+        writer = csv.DictWriter(f, fieldnames=csv_headers)
+        writer.writeheader()
+        for i, value in enumerate(final_hash):
+            writer.writerow({csv_headers[0]: final_hash[i],  # Commit_hash
+                             csv_headers[1]: final_score[i]})  # Commit_ADD_DEL
+    logger.info(f'Final average Commit: {project_name} ✔')
 
 
 def average_commit(urls, average_file_type, verbose):
     """ Invoca metodo di analisi: Average Commits """
     # Setting log
     log(verbose)
+
+    # csv header
+    csv_headers = ["Commit_hash", "ADD+DEL"]
 
     # Indice del repo corrente sotto analisi
     repo_index = 0
@@ -162,16 +189,11 @@ def average_commit(urls, average_file_type, verbose):
         git = Git(commit.project_path)
         logger.debug(f'Project: {commit.project_name} #Commits: {git.total_commits()}')  # total commits
         if verbose:  # log file + console
-            log_view(url, commit.project_name, git.total_commits(), average_file_type)
+            log_view(url, commit.project_name, git.total_commits(), average_file_type, csv_headers)
         else:  # log file
-            bar_view(url, commit.project_name, git.total_commits(), average_file_type)
+            bar_view(url, commit.project_name, git.total_commits(), average_file_type, csv_headers)
         repo_index += 1
 
         csv_sort(commit.project_name)   # ordino per ADD+DEL
-        csv_avg(commit.project_name)    # calcolo la media
+        csv_avg(commit.project_name, csv_headers)    # calcolo la media
 
-    # Stampo a video il risultato
-    # print_tabulate(rep_list, headers, average_matrix)
-
-    # CSV file
-    # csv_generation(rep_list, headers, average_matrix)
