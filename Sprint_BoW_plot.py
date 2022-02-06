19bf93bcf54984f169a68e4ebe68024d82017893
Flik1 <leo.pelonero@hotmail.it>
2022-02-06 02:18:20 +0100

    :bug: Sprint_BoW_plot.py plot, need an if-ckeck

--- Sprint_BoW_plot.py
+++ Sprint_BoW_plot.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import csv
+import os
+import matplotlib.pyplot as plt
 import spacy
+import seaborn as sns
 from spacy.matcher import Matcher
 import nltk
 from nltk.stem.porter import *
@@ -20,8 +23,8 @@ path = input("Enter CSV Repositories: ")
 # oppure path = 'data-results/sprint_week_nomerepository.csv'
 path_split = path.split('/')
 
-data = pd.read_csv(path)  # prendo i dati
-data = data[pd.notnull(data['Msg_data'])]  # checking not missing msg
+data_sprint = pd.read_csv(path)  # prendo i dati
+data_sprint = data_sprint[pd.notnull(data_sprint['Msg_data'])]  # checking not missing msg
 #print(data.head(10))
 
 
@@ -34,7 +37,7 @@ def clean_text(text):
     return text
 
 
-data['Msg_data'] = data['Msg_data'].apply(clean_text)
+data_sprint['Msg_data'] = data_sprint['Msg_data'].apply(clean_text)
 #print(data['Msg_data'][0])
 
 # ---------------
@@ -48,15 +51,15 @@ csv_headers = ["Day", "Week", "Msg_data"]
 with open("testset.csv", 'w') as f:
     writer = csv.DictWriter(f, fieldnames=csv_headers)
     writer.writeheader()
-    for i, line in enumerate(data['Msg_data']):
+    for i, line in enumerate(data_sprint['Msg_data']):
         word_str = ""
         for word in line.split():
             word_str += stemmer.stem(word) + " "
-        writer.writerow({'Day': data['Day'][i], 'Week': data['Week'][i], 'Msg_data': word_str})
+        writer.writerow({'Day': data_sprint['Day'][i], 'Week': data_sprint['Week'][i], 'Msg_data': word_str})
 f.close()
 
 # ---------------
-data = pd.read_csv("testset.csv")
+data_sprint = pd.read_csv("testset.csv")
 nlp = spacy.load('en_core_web_sm')
 
 m_tool = Matcher(nlp.vocab)
@@ -94,7 +97,7 @@ with open("finale.csv", 'w') as f:
     writer.writeheader()
 f.close()
 
-for index, row in data.iterrows():
+for index, row in data_sprint.iterrows():
     sentence = nlp(row['Msg_data'])
     phrase_matches = m_tool(sentence)
     for match_id, start, end in phrase_matches:
@@ -107,28 +110,81 @@ for index, row in data.iterrows():
             f.close()
 
 # -----------------------
+# bow + tag count
+data_count = pd.read_csv('finale.csv')
 
-topa = pd.read_csv('finale.csv')
-"""counted = topa.groupby(["Day", "Week"])["Tag"].value_counts()
-topa.to_csv("counted.csv")"""
+data_count_head = ["Day", "Week", "Tag", "#Tag"]
 
-leohead = ["Day","Week","Tag","#Tag"]
-
-week_grouped = topa.groupby(["Day", "Week"])["Tag"].value_counts()
-with open("leo.csv", 'w') as f:
-    writer = csv.DictWriter(f, fieldnames=leohead)
+week_grouped = data_count.groupby(["Day", "Week"])["Tag"].value_counts()
+with open("bow_tag.csv", 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=data_count_head)
     writer.writeheader()
 f.close()
 
-poppe = pd.DataFrame(week_grouped)
-poppe.to_csv("leo.csv", header=False, mode="a")
-#poppe.to_csv('leo.csv')
+bow = pd.DataFrame(week_grouped)
+bow.to_csv("bow_tag.csv", header=False, mode="a")
 
-"""for name, group in df.groupby(["Day", "Week"]):
-    print('group name:', name)
-    print('group rows:')
-    print(group)
-    print('counts of Quality values:')
-    print(group["Tag"].value_counts())
+# -----------------------
+# bow tag filter multiple tag sprint to a single tag
 
-print(df.head(10))"""
+data_count_filter = pd.read_csv('bow_tag.csv')
+
+with open("bow_tag_filter.csv", 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=data_count_head)
+    writer.writeheader()
+    for i, line in enumerate(data_count_filter['Day']):  # ciclo sui giorni
+        if i == 0:
+            prec = line
+            max_index = i
+            continue
+        if prec == line:   # giorni uguali con tag diversi
+            #print(i, "uguali: ", prec, " - ", line)
+            if data_count_filter['#Tag'][i-1] < data_count_filter['#Tag'][i]:
+                prec = line
+                max_index = i
+        else:   # diversi salvo il precedente
+            #print(i, "diversi: ", prec, " - ", line, " salvo")
+            writer.writerow({'Day': data_count_filter['Day'][max_index], 'Week': data_count_filter['Week'][max_index],
+                             'Tag': data_count_filter['Tag'][max_index], '#Tag': data_count_filter['#Tag'][max_index]})
+            prec = line
+            max_index = i
+
+        if i == len(data_count_filter['Day'])-1:  # devo salvare l'ultimo
+            # print(i, "ultimo")
+            #if data_count_filter['Day'][i] != data_count_filter['Day'][i-1]:  # se diverso dal penultimo - salvo
+            writer.writerow({'Day': data_count_filter['Day'][max_index], 'Week': data_count_filter['Week'][max_index],
+                         'Tag': data_count_filter['Tag'][max_index], '#Tag': data_count_filter['#Tag'][max_index]})
+f.close()
+
+os.remove('testset.csv')
+os.remove('finale.csv')
+os.remove('bow_tag.csv')
+
+# ----------------
+# Join scrum con gli sprint derivati dal BoW tag
+
+data_bow = pd.read_csv("bow_tag_filter.csv")  # prendo i dati bow
+
+# richiedo i dati del main scrum sprint
+main_path = input("Enter CSV Repositories Main to join: ")
+text_path_split = main_path.split('/')
+main_data = pd.read_csv(main_path) # prendo i dati del branch text
+
+main_datax = [x[:4] +"-"+ str(y) for x, y in zip(main_data['Day'], main_data['Week'])]     # dati nella forma anno-settimana
+datab = [x[:4] +"-" + str(y) for x, y in zip(data_bow['Day'], data_bow['Week'])]
+
+sns.set()   # corrisponde al plt.grid(True)
+
+plt.figure(1)
+barlist = plt.bar(main_datax, main_data['Sprint_week'])
+for indice, valore in enumerate(main_datax):
+    if valore in datab:
+        print("dentro ",data_bow[datab.index(valore)])
+        if data_bow[datab.index(valore)] in ['FIX', 'TEST', 'BUG', 'REF', 'DOC']:
+            barlist[indice].set_color('g')
+plt.xticks(main_datax, main_datax, rotation=25)     # x
+plt.xlabel('Weekly commits')        # x
+plt.ylabel('Number of changes')     # y
+plt.suptitle(path_split[len(path_split) - 1], fontsize=10)
+plt.title('Sprint Weekly trend BoW', fontsize=15)
+plt.show()
